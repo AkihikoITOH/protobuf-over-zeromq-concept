06540aa8d393d79b0c0bc8e65bcf7b0b18cc92a0
AkihikoITOH <akihiko.mus+github@gmail.com>
2019-02-11 18:39:11 +0100

    refactor

--- client/main.go
+++ client/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"io/ioutil"
 	"log"
-	"sync"
 
 	"github.com/AkihikoITOH/protobuf-over-zeromq-concept/client/pb"
 	"github.com/Pallinder/sillyname-go"
@@ -20,45 +19,50 @@ func newUser() *pb.User {
 	return &pb.User{Uuid: uuid.Must(uuid.NewV4()).String(), Name: sillyname.GenerateStupidName()}
 }
 
-func main() {
+func setupListener() (*Listener, error) {
 	listener, err := NewListener(lgr)
 	if err != nil {
-		log.Fatal(err.Error())
+		return nil, err
+	}
+	listener.Setup(DefaultListenerEndpoint)
+
+	return listener, nil
+}
+
+func setupPublisher() (*Publisher, error) {
+	publisher, err := NewPublisher(lgr)
+	if err != nil {
+		return nil, err
+	}
+
+	publisher.Setup(DefaultPublisherEndpoint)
+
+	return publisher, nil
+}
+
+func main() {
+	listener, err := setupListener()
+	if err != nil {
+		lgr.Fatal(err.Error())
 	}
 	defer listener.Close()
 
-	publisher, err := NewPublisher(lgr)
+	publisher, err := setupPublisher()
 	if err != nil {
-		log.Fatal(err.Error())
+		lgr.Fatal(err.Error())
 	}
 	defer publisher.Close()
 
-	listener.Setup(DefaultListenerEndpoint)
-	publisher.Setup(DefaultPublisherEndpoint)
-	ctx, cancel := context.WithCancel(context.Background())
+	listenCtx, cancelListen := context.WithCancel(context.Background())
+	defer cancelListen()
 
-	listenerWg := &sync.WaitGroup{}
+	publishCtx, cancelPublish := context.WithCancel(context.Background())
+	defer cancelPublish()
 
-	listenerWg.Add(1)
-	go func() {
-		defer listenerWg.Done()
-		listener.Listen(ctx)
-	}()
+	go listener.Listen(listenCtx)
+	go publisher.Poll(publishCtx)
 
-	outgoingMessages := make(chan *pb.Message)
-
-	go func() {
-		for {
-			msg, ok := <-outgoingMessages
-			if !ok {
-				cancel()
-				break
-			}
-			publisher.Publish(msg)
-		}
-	}()
-
-	view := NewView(newUser(), listener.Messages(), outgoingMessages)
+	view := NewView(newUser(), listener.Messages(), publisher.Messages())
 	if err != nil {
 		log.Fatal(err.Error())
 	}
--- client/publisher.go
+++ client/publisher.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"github.com/AkihikoITOH/protobuf-over-zeromq-concept/client/pb"
 	"github.com/golang/protobuf/proto"
 	"github.com/google/logger"
@@ -11,7 +13,9 @@ const DefaultPublisherEndpoint = "tcp://127.0.0.1:8080"
 
 type Publisher struct {
 	*zmq.Socket
-	logger *logger.Logger
+	messageChannel chan *pb.Message
+	errorChannel   chan error
+	logger         *logger.Logger
 }
 
 func NewPublisher(logger *logger.Logger) (*Publisher, error) {
@@ -21,7 +25,12 @@ func NewPublisher(logger *logger.Logger) (*Publisher, error) {
 		return nil, err
 	}
 
-	return &Publisher{socket, logger}, nil
+	return &Publisher{
+		socket,
+		make(chan *pb.Message),
+		make(chan error),
+		logger,
+	}, nil
 }
 
 func (publisher *Publisher) Setup(endpoint string) error {
@@ -34,13 +43,40 @@ func (publisher *Publisher) Setup(endpoint string) error {
 	return nil
 }
 
-func (publisher *Publisher) Publish(message *pb.Message) error {
+func (publisher *Publisher) Messages() chan<- *pb.Message {
+	return publisher.messageChannel
+}
+
+func (publisher *Publisher) Errors() <-chan error {
+	return publisher.errorChannel
+}
+
+func (publisher *Publisher) publish(message *pb.Message) error {
 	publisher.logger.Infof("Publishing message: %s", message)
 	data, err := proto.Marshal(message)
 	if err == nil {
 		publisher.SendBytes(data, 0)
 	}
 	return err
+}
+
+func (publisher *Publisher) Poll(ctx context.Context) {
+	defer close(publisher.messageChannel)
+	defer close(publisher.errorChannel)
+
+	done := false
+	for !done {
+		select {
+		case <-ctx.Done():
+			done = true
+			break
+		case msg, _ := <-publisher.messageChannel:
+			err := publisher.publish(msg)
+			if err != nil {
+				publisher.errorChannel <- err
+			}
+		}
+	}
 }
 
 func (publisher *Publisher) Close() {
